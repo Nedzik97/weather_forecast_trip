639aec1628f1391629239b32b0b627678512627d
Nedzik <113420019+Nedzik97@users.noreply.github.com>
2024-02-16 18:42:09 +0200

    create a new project weather-forecast

---
+++ src/components/app/app.jsx
@@ -0,0 +1,7 @@
+import styles from './app.module.scss';
+
+export const App = () => {
+	return (
+		<div className={styles.app}>123</div>
+	)
+}
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import {App} from './components/app/app'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
